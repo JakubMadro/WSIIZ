ecf3287d25f23e074c7fa16b61d67ff348447295
Jakub Mądro <w67161@student.wsiz.edu.pl>
2023-10-23 19:37:25 +0200

    Lab01 - skonczone

--- Labo - Programowanie Obiektowe - Ewa Zeslawska/Lab01/Lab_01/Program.cs
+++ Labo - Programowanie Obiektowe - Ewa Zeslawska/Lab01/Lab_01/Program.cs
@@ -11,19 +11,23 @@
 //    Console.Write(i + " ");
 //}
 
-//Zadanie1();
-//Zadanie2();
+
 //bool stan = true;
 //while (stan==true) {
 //    Zadanie2();
 //}
+Zadanie1();
+Zadanie2();
 Zadanie3();
-
-
+Zadanie4();
+Zadanie5();
+Zadanie6();
+Zadanie7();
 
 ////Koniec Main 
 
 //Tutaj są przykłady funkcji 
+
 
 void WriteExample()
 {
@@ -54,6 +58,7 @@ void View(string name, double value)
 
 void Zadanie1()
 {
+    Console.WriteLine("\n\n\n==============ZADANIE 1==============\n\n\n");
     double a = InputDouble(), b = InputDouble(), c = InputDouble();
     double delta = Math.Pow(b, 2) - (4 * a * c);
     double x1, x2;
@@ -76,6 +81,7 @@ void Zadanie1()
 //Zadanie 2 
 void Zadanie2()
 {
+    Console.WriteLine("\n\n\n==============ZADANIE 2==============\n\n\n");
     Menu();
 }
 
@@ -165,19 +171,38 @@ void Wyjscie()
 
 void Zadanie3()
 {
+    Console.WriteLine("\n\n\n==============ZADANIE 3==============\n\n\n");
     double[] table = new double[10];
+    AddToArray(table);
 
 
     Console.WriteLine("1. Wyświetlanie tablicy od pierwszego do ostatniego indeksu ");
     Console.WriteLine("2. Wyświetlanie tablicy od ostatniego do pierwszego indeksu.");
-    Console.WriteLine("3. Wyświetlanie elementów o nieparzystych indeksach.");
-    Console.WriteLine("4. Wyświetlanie elementów o parzystych indeksach.");
+    Console.WriteLine("3. Wyświetlanie elementów o parzystych indeksach.");
+    Console.WriteLine("4. Wyświetlanie elementów o nieparzystych indeksach.");
 
-    AddToArray(table);
-    FromFirstToLast(table);
-    FromLastToFirst(table);
-    DisplayEvenNumbers(table);
-    DisplayOddNumbers(table);
+    double choice;
+    do
+    {
+        choice = InputDouble();
+        if (choice == 1)
+        {
+            FromFirstToLast(table);
+        }
+        else if (choice == 2)
+        {
+            FromLastToFirst(table);
+        }
+        else if (choice == 3)
+        {
+            DisplayEvenNumbers(table);
+        }
+        else if (choice == 4)
+        {
+            DisplayOddNumbers(table);
+        }
+
+    } while (choice != 5);
 
 
 }
@@ -225,4 +250,145 @@ void DisplayOddNumbers(double[] table)
     {
         Console.Write(table[i] + ", ");
     }
+}
+//Koniec Zadania 3 
+
+//Zadanie 4 
+
+void Zadanie4()
+{
+    Console.WriteLine("\n\n\n==============ZADANIE 4==============\n\n\n");
+    double[] table = new double[10];
+    AddToArray(table);
+    CalculateArrSum(table);
+    CalculateArrProduct(table);
+    CalculateArrAvg(table);
+    FindMinValue(table);
+    FindMaxValue(table);
+
+}
+
+double CalculateArrSum(double[] table)
+{
+    double sum = 0;
+    foreach (double value in table) { sum += value; };
+    Console.WriteLine("Suma elementów Tablicy Wynosi: " + sum);
+    return sum;
+
+}
+
+void CalculateArrProduct(double[] table)
+{
+    double product = 1;
+    foreach (double value in table) { product *= value; };
+    Console.WriteLine("Iloczyn Elementów Tablicy wynosi: " + product);
+}
+
+void CalculateArrAvg(double[] table)
+{
+    double sum = CalculateArrSum(table);
+    double avg = sum / table.Length;
+    Console.WriteLine("Wartość średnia wynosi: " + avg);
+}
+
+void FindMinValue(double[] table)
+{
+    double minValue = 0;
+    foreach (double value in table)
+    {
+        if (value < minValue) minValue = value;
+    }
+    Console.WriteLine("Najmniejsza wartość to: " + minValue);
+}
+void FindMaxValue(double[] table)
+{
+    double maxValue = 0;
+    foreach (double value in table)
+    {
+        if (value > maxValue) maxValue = value;
+    }
+    Console.WriteLine("Najwieksza wartość to: " + maxValue);
+}
+
+//Koniec Zadania 4 
+
+//Zadanie 5 
+
+void Zadanie5()
+{
+    Console.WriteLine("\n\n\n==============ZADANIE 5==============\n\n\n");
+    for (int i = 20; i >= 0; i--)
+    {
+        if (i == 2 || i == 6 || i == 9 || i == 15 || i == 19)
+        {
+            continue;
+        }
+        Console.Write(i + ", ");
+    }
+}
+
+void Zadanie6()
+{
+    Console.WriteLine("\n\n\n==============ZADANIE 6==============\n\n\n");
+    while (true)
+    {
+        Console.WriteLine("Podaj Liczby calkowite");
+        int number = (int)InputDouble();
+        if (number < 0) break;
+    }
+}
+
+void Zadanie7()
+{
+    Console.WriteLine("\n\n\n==============ZADANIE 7==============\n\n\n");
+
+    Console.WriteLine("Podaj Ile liczb chcesz pisac");
+    int n = (int)InputDouble();
+    double[] numbers = new double[n];
+    AddNToArr(numbers, n);
+    Console.WriteLine("Liczby Przed sortowaniem:");
+    ShowArr(numbers);
+    BubbleSort(numbers);
+    Console.WriteLine("\n Liczby Po sortowaniem:");
+    ShowArr(numbers);
+
+}
+
+void AddNToArr(double[] numbers, int n)
+{
+    Console.WriteLine("Podaj Wartości do wpisania do Tablicy");
+    for (int i = 0; i < n; i++)
+    {
+        numbers[i] = InputDouble();
+    }
+}
+
+void ShowArr(double[] numbers)
+{
+    foreach (var element in numbers)
+    {
+        Console.Write(element + ", ");
+    }
+}
+
+void BubbleSort(double[] numbers)
+{
+    int n = numbers.Length;
+    bool changed;
+    do
+    {
+        changed = false;
+        for (int i = 1; i < n; i++)
+        {
+            if (numbers[i - 1] > numbers[i])
+            {
+                double temp = numbers[i - 1];
+                numbers[i - 1] = numbers[i];
+                numbers[i] = temp;
+                changed = true;
+            }
+
+        }
+
+    } while (changed);
 }
