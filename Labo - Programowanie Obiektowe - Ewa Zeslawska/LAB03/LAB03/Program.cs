c8ba95d02db553f94794d23e029afe0dbd963162
Jakub Mądro <w67161@student.wsiz.edu.pl>
2023-11-26 19:21:31 +0100

    Zadanie 2

--- Labo - Programowanie Obiektowe - Ewa Zeslawska/LAB03/LAB03/Program.cs
+++ Labo - Programowanie Obiektowe - Ewa Zeslawska/LAB03/LAB03/Program.cs
@@ -1,8 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LAB03;
-
-
-
+using System.ComponentModel.Design;
 
 Person[] people = new Person[]
 {
@@ -23,7 +21,33 @@ Book[] books = new Book[]
     new Book("Tytuł 4", people[3], new DateTime(2020,04,05))
 };
 
-foreach (Book item in books)
+Reader[] readers = new Reader[people.Length];
+for (int i = 0; i < readers.Length; i++)
 {
-    item.View();
+    readers[i] = new Reader(people[i].FirstName, people[i].LastName, people[i].Age);
+    if (i == 0)
+        readers[i].Books = new Book[] { books[randInt()] };
+    else if (i == 1)
+        readers[i].Books = new Book[] { books[randInt()], books[randInt()] };
+    else
+        readers[i].Books = new Book[] { books[randInt()], books[randInt()], books[randInt()] };
 }
+//wywołanie metody viewBook
+
+foreach (var item in readers)
+{
+    item.ViewBook();
+}
+
+//metoda do losowania 
+
+int randInt()
+{
+    Random random = new Random();
+    return random.Next(books.Length);
+}
+
+//foreach (Book item in books)
+//{
+//    item.View();
+//}
---
+++ Labo - Programowanie Obiektowe - Ewa Zeslawska/LAB03/LAB03/Reader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB03
+{
+    internal class Reader : Person
+    {
+        public Book[] Books { get; set; }
+
+        public Reader(string firstname, string lastname, int age):base(firstname,lastname,age)
+        { 
+        }
+
+        public void ViewBook()
+        {
+            Console.WriteLine($"{FirstName} {LastName} przeczytał(a) następujące książki:");
+
+            foreach (var book in Books)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+
+    }
+}
