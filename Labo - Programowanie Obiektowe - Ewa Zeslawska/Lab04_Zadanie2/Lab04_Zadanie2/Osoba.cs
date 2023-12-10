332fc3dbe727e208472bd397963d739e69f16a10
Jakub Mądro <w67161@student.wsiz.edu.pl>
2023-12-10 10:55:56 +0100

    Zadanie 2.1

---
+++ Labo - Programowanie Obiektowe - Ewa Zeslawska/Lab04_Zadanie2/Lab04_Zadanie2/Osoba.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04_Zadanie2
+{
+    internal class Osoba
+    {
+        public string Imie;
+        public string Nazwisko;
+        public string Pesel;
+
+        public void SetFirstName(string firstName)
+        {
+            Imie = firstName;
+        }
+
+        public void SetLastName(string lastName)
+        {
+            Nazwisko = lastName;
+        }
+        public void SetPesel(string pesel)
+        {
+            Pesel = pesel;
+        }
+
+        public int GetAge()
+        {
+            int birtYear = int.Parse(Pesel.Substring(0, 2));
+            int currentYear = DateTime.Now.Year;
+            int age = currentYear - (birtYear+1900 +100);
+            return age;
+        }
+        public string GetGender()
+        {
+
+            if (Pesel[9] % 2 == 0)
+            {
+                return "Female";
+            }
+            else
+            {
+                return "Male";
+            }
+        }
+
+        public string GetFullName()
+        {
+            return $"{Imie} {Nazwisko}";
+        }
+
+        public bool CanGoAloneToHome()
+        {
+            if (GetAge() < 12)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public string GetEducationInfo()
+        {
+            int age = GetAge();
+
+            if (age < 7)
+            {
+                return "Przedszkole";
+            }
+            else if (age < 16)
+            {
+                return "Podstawówka";
+            }
+            else if (age < 19)
+            {
+                return "Średnia";
+            }
+            else
+            {
+                return "Student";
+            }
+        }
+    }
+}
---
+++ Labo - Programowanie Obiektowe - Ewa Zeslawska/Lab04_Zadanie2/Lab04_Zadanie2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab04_Zadanie2;
+using System;
+
+Console.WriteLine("Hello, World!");
+
+Osoba person = new Osoba();
+
+person.SetFirstName("Maks");
+person.SetLastName("Kmieć");
+person.SetPesel("1501011233");
+
+
+
+Console.WriteLine($"Imie i Nazwisko: {person.GetFullName()}");
+Console.WriteLine($"Wiek: {person.GetAge()}");
+Console.WriteLine($"Płeć: {person.GetGender()}");
+Console.WriteLine($"Szkoła: {person.GetEducationInfo()}");
+Console.WriteLine($"Czy może iśc sam do domu: {person.CanGoAloneToHome()}");
